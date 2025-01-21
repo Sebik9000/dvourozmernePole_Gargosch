a14b97f9d4dd5df57b89f601784603d6af148712
Sebastian Gargosch <98969@student.vitkovickastredni.cz>
2025-01-21 12:06:30 +0100

    add_row

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ pole = [
     [4, 105, 6],
     [7, 8, 9]
 ]
+
+# Nebyl jsem si jistý jak jste to chtěla, šlo to udělat tak že bych prostě přidal nový řádek a sloupec do toho samotnýho pole ale takhle to je lepší si myslím. 
+
+# Extra řádek
+novy_radek = [10, 11, 12]
+pole.append(novy_radek)
+
+# Extra sloupec 
+novy_sloupec = [13, 14, 15, 16]
+for i in range(len(pole)):
+    pole[i].append(novy_sloupec[i])
+
+print(pole)
